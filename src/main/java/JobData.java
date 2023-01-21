4b2cc0f83c5049f552e261642f3acf470660b0d9
Andy <Afisch2014@gmail.com>
2023-01-21 01:32:45 -0800

    randomly realized i hadn't been doing git commit so id decided to start.
    here are all my internal notes I was taking
     //how to get each value available?
            // first we need to take all jobs into key value pairs -- to do that we need a hashmap
            //now that we have the hashmap -- we must then isolate all of our values
            //Now at this point we can kind of match! We want to see if JOB contains any to our keyvalue pairs. We can do that with an If statement
            //keyValues is of type Entry. which we can't do much with. However we can make those entries a string
    
            //Since KeyValues isn't in scope of the outer loop -- we can add the KeyValues to something else to compare -- perhaps an arrayList of Strings
            //So testArrayList has been populated by single key value pairs. I want to go ahead and make those key value pairs the full job.
            //great now that you are making full job listings - it is time to see if you can write code to only print the listing IF it contains a specific value--
            // that specific value is fed into the above method with the word "value"
            //I'm realizing that perhaps I may have to take the stringKeyValues and write an if else statement to add them to an arrayList. The purpose of the array list is to be used to compare to the value (if arraylist does not contain value, add that value)
            //closer to success! -- notes: using the value front -- we get 10 hits(well, 20) Those 10 hits repeat the same job(must look into loop)
            //note -- had trouble printing ALL job listings into a single array.  -- now get back to work at matching

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,8 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -60,12 +58,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -91,16 +89,42 @@ public class JobData {
      * Search all columns for the given term
      *
      * @param value The search term to look for
-     * @return      List of all jobs with at least one field containing the value
+     * @return List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
-
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> doesSearchValueMatch = new ArrayList<>();
 
+        //how to get each value available?
+        // first we need to take all jobs into key value pairs -- to do that we need a hashmap
+        //now that we have the hashmap -- we must then isolate all of our values
+        //Now at this point we can kind of match! We want to see if JOB contains any to our keyvalue pairs. We can do that with an If statement
+        //keyValues is of type Entry. which we can't do much with. However we can make those entries a string
+
+        //Since KeyValues isn't in scope of the outer loop -- we can add the KeyValues to something else to compare -- perhaps an arrayList of Strings
+        //So testArrayList has been populated by single key value pairs. I want to go ahead and make those key value pairs the full job.
+        //great now that you are making full job listings - it is time to see if you can write code to only print the listing IF it contains a specific value--
+        // that specific value is fed into the above method with the word "value"
+        //I'm realizing that perhaps I may have to take the stringKeyValues and write an if else statement to add them to an arrayList. The purpose of the array list is to be used to compare to the value (if arraylist does not contain value, add that value)
+        //closer to success! -- notes: using the value front -- we get 10 hits(well, 20) Those 10 hits repeat the same job(must look into loop)
+        //note -- had trouble printing ALL job listings into a single array.  -- now get back to work at matching
+
+        ArrayList<String> testArrayList = new ArrayList<>();
+        String stringKeyValues = null;
+        for (HashMap<String, String> job : allJobs) {
+            stringKeyValues = "\n*****\n";
+            for (Map.Entry<String, String> keyValues : job.entrySet()) {
+                stringKeyValues += (keyValues.getKey() + ": " + keyValues.getValue() + "\n");
+            }
+            testArrayList.add(stringKeyValues);
+        }
+
+        System.out.println(testArrayList);
         // TODO - implement this method
-        return null;
+        return doesSearchValueMatch;
     }
+
 
     /**
      * Read in data from a CSV file and store it in a list
--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.sql.SQLOutput;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -10,7 +8,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -60,6 +58,7 @@ public class TechJobs {
                 // What is their search term?
                 System.out.println("\nSearch term:");
                 String searchTerm = in.nextLine();
+
 
                 if (searchField.equals("all")) {
                     printJobs(JobData.findByValue(searchTerm));
@@ -112,14 +111,29 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
+        
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+            return;
+        }
+        for (HashMap<String, String> jobHashMap : someJobs) {
+            String jobInfo = "\n*****\n";
+            for (Map.Entry<String, String> infoPair : jobHashMap.entrySet()) {
+                jobInfo += (infoPair.getKey() + ": " + infoPair.getValue() + "\n");
+            }
+            jobInfo += "*****";
+            System.out.println(jobInfo);
+
+        }
     }
+
+
 }
